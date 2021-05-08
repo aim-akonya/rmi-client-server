3df05d28c27d61c15a107e3dff7cd25fe7aa20ae
aim-akonya <aim.akonya8@gmail.com>
2021-05-08 22:36:09 +0300

    client setup

--- client/src/main/java/com/aim/app/Application.java
+++ client/src/main/java/com/aim/app/Application.java
@@ -1,8 +1,32 @@
 package com.aim.app;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import com.aim.app.rmi.Hello;
+
 public class Application {
 
+	public Application() {
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Application Startup Reached");
+		try {
+
+			// Getting the registry
+			Registry registry = LocateRegistry.getRegistry(null);
+
+			// Looking up the registry for the remote object
+			Hello stub = (Hello) registry.lookup("Hello");
+
+			// Calling the remote method using the obtained object
+			stub.printMsg();
+
+			// System.out.println("Remote method invoked");
+
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
 	}
 }
---
+++ client/src/main/java/com/aim/app/rmi/Hello.java
@@ -0,0 +1,7 @@
+package com.aim.app.rmi;
+
+import java.rmi.RemoteException;
+
+public interface Hello {
+	void printMsg() throws RemoteException;
+}
